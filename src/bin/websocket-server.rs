d0d44a4375f4cd9d919fe51b49fd67aa8805fee9
Giovanni <pino_otto@yahoo.com>
2025-01-11 18:43:20 +0800

    fixing save to disk (wip)

--- src/bin/websocket-server.rs
+++ src/bin/websocket-server.rs
@@ -26,6 +26,7 @@ use axum_extra::TypedHeader;
 
 use chrono::Local;
 use std::borrow::Cow;
+use std::io;
 use std::ops::ControlFlow;
 use std::{net::SocketAddr, path::PathBuf};
 use tokio::{
@@ -105,6 +106,7 @@ async fn ws_handler(
 
 /// Actual websocket statemachine (one will be spawned per connection)
 async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
+    let mut serial_number = String::from("undefined");
     // send a ping (unsupported by some browsers) just to kick things off and get a response
     if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
         println!("Pinged {who}...");
@@ -115,13 +117,29 @@ async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
         return;
     }
 
+    if let Some(msg) = socket.recv().await {
+        if let Ok(msg) = msg {
+            let message = get_text(msg, who);
+            if message.is_break() {
+                return;
+            } else {
+                if let Some(text) = message.continue_value() {
+                    serial_number = text.clone();
+                }
+            }
+        } else {
+            println!("client {who} abruptly disconnected");
+            return;
+        }
+    }
+
     // receive single message from a client (we can either receive or send with socket).
     // this will likely be the Pong for our Ping or a hello message from client.
     // waiting for message from a client will block this task, but will not block other client's
     // connections.
     while let Some(msg) = socket.recv().await {
         if let Ok(msg) = msg {
-            if process_message(msg, who).is_break() {
+            if process_message(msg, who, &serial_number).await.is_break() {
                 return;
             }
         } else {
@@ -130,92 +148,40 @@ async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
         }
     }
 
-    // // Since each client gets individual statemachine, we can pause handling
-    // // when necessary to wait for some external event (in this case illustrated by sleeping).
-    // // Waiting for this client to finish getting its greetings does not prevent other clients from
-    // // connecting to server and receiving their greetings.
-    // for i in 1..5 {
-    //     if socket
-    //         .send(Message::Text(format!("Hi {i} times!")))
-    //         .await
-    //         .is_err()
-    //     {
-    //         println!("client {who} abruptly disconnected");
-    //         return;
-    //     }
-    //     tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-    // }
-    //
-    // // By splitting socket we can send and receive at the same time. In this example we will send
-    // // unsolicited messages to client based on some sort of server's internal event (i.e .timer).
-    // let (mut sender, mut receiver) = socket.split();
-    //
-    // // Spawn a task that will push several messages to the client (does not matter what client does)
-    // let mut send_task = tokio::spawn(async move {
-    //     let n_msg = 20;
-    //     for i in 0..n_msg {
-    //         // In case of any websocket error, we exit.
-    //         if sender
-    //             .send(Message::Text(format!("Server message {i} ...")))
-    //             .await
-    //             .is_err()
-    //         {
-    //             return i;
-    //         }
-    //
-    //         tokio::time::sleep(std::time::Duration::from_millis(300)).await;
-    //     }
-    //
-    //     println!("Sending close to {who}...");
-    //     if let Err(e) = sender
-    //         .send(Message::Close(Some(CloseFrame {
-    //             code: axum::extract::ws::close_code::NORMAL,
-    //             reason: Cow::from("Goodbye"),
-    //         })))
-    //         .await
-    //     {
-    //         println!("Could not send Close due to {e}, probably it is ok?");
-    //     }
-    //     n_msg
-    // });
-    //
-    // // This second task will receive messages from client and print them on server console
-    // let mut recv_task = tokio::spawn(async move {
-    //     let mut cnt = 0;
-    //     while let Some(Ok(msg)) = receiver.next().await {
-    //         cnt += 1;
-    //         // print message and break if instructed to do so
-    //         if process_message(msg, who).is_break() {
-    //             break;
-    //         }
-    //     }
-    //     cnt
-    // });
-    //
-    // // If any one of the tasks exit, abort the other.
-    // tokio::select! {
-    //     rv_a = (&mut send_task) => {
-    //         match rv_a {
-    //             Ok(a) => println!("{a} messages sent to {who}"),
-    //             Err(a) => println!("Error sending messages {a:?}")
-    //         }
-    //         recv_task.abort();
-    //     },
-    //     rv_b = (&mut recv_task) => {
-    //         match rv_b {
-    //             Ok(b) => println!("Received {b} messages"),
-    //             Err(b) => println!("Error receiving messages {b:?}")
-    //         }
-    //         send_task.abort();
-    //     }
-    // }
-
     // returning from the handler closes the websocket connection
     println!("Websocket context {who} destroyed");
 }
 
+fn get_text(msg: Message, who: SocketAddr) -> ControlFlow<(), String> {
+    match msg {
+        Message::Text(text) => {
+            println!(">>> {who} sent str: {text:?}");
+            ControlFlow::Continue(text)
+        }
+        Message::Close(c) => {
+            if let Some(cf) = c {
+                println!(
+                    ">>> {} sent close with code {} and reason `{}`",
+                    who, cf.code, cf.reason
+                );
+            } else {
+                println!(">>> {who} somehow sent close message without CloseFrame");
+            }
+            return ControlFlow::Break(());
+        }
+        _ => {
+            println!("unexpected message");
+            return ControlFlow::Break(());
+        }
+    }
+}
+
 /// helper to print contents of messages to stdout. Has special treatment for Close.
-fn process_message(msg: Message, who: SocketAddr) -> ControlFlow<(), ()> {
+async fn process_message(
+    msg: Message,
+    who: SocketAddr,
+    serial_number: &str,
+) -> ControlFlow<(), ()> {
     match msg {
         Message::Text(t) => {
             println!(">>> {who} sent str: {t:?}");
@@ -223,6 +189,7 @@ fn process_message(msg: Message, who: SocketAddr) -> ControlFlow<(), ()> {
         Message::Binary(d) => {
             println!(">>> {} sent {} bytes: {:?}", who, d.len(), d);
             println!("going to save received image to file");
+            let _ = save_image(serial_number, d).await;
             // to do
             // save the image to disk
         }
@@ -251,40 +218,46 @@ fn process_message(msg: Message, who: SocketAddr) -> ControlFlow<(), ()> {
     ControlFlow::Continue(())
 }
 
-async fn save_image(serial_number: &str) -> Result<(), String> {
-    let local_time = Local::now().format("%Y%m%d-%H%M%S");
-    let filename = format!("image-{}.jpg", local_time);
+async fn save_image(serial_number: &str, data: Vec<u8>) -> Result<(), String> {
+    async {
+        let local_time = Local::now().format("%Y%m%d-%H%M%S");
+        let filename = format!("image-{}.jpg", local_time);
 
-    tokio::fs::create_dir_all(format!("{}/{}", UPLOADS_DIRECTORY, serial_number))
-        .await
-        .expect("failed to create `uploads/<serial_number>` directory");
+        tokio::fs::create_dir_all(format!("{}/{}", UPLOADS_DIRECTORY, serial_number))
+            .await
+            .expect("failed to create `uploads/<serial_number>` directory");
 
-    let path = format!("{}/{}", serial_number, filename);
+        let path = format!("{}/{}", serial_number, filename);
 
-    // Create the file. `File` implements `AsyncWrite`.
-    let path_buf = std::path::Path::new(UPLOADS_DIRECTORY).join(&path);
-    let mut file = BufWriter::new(File::create(path_buf).await?);
+        // Create the file. `File` implements `AsyncWrite`.
+        let path_buf = std::path::Path::new(UPLOADS_DIRECTORY).join(&path);
+        let mut file = BufWriter::new(File::create(path_buf).await?);
 
-    // Copy the body into the file.
-    tokio::io::copy(&mut body_reader, &mut file).await?;
+        // Copy the body into the file.
+        tokio::io::copy(&mut data.as_slice(), &mut file).await?;
 
-    // Read the file just copied
-    let path_buf = std::path::Path::new(UPLOADS_DIRECTORY).join(&path);
-    let mut image_file = BufReader::new(File::open(path_buf).await?);
+        // Read the file just copied
+        let path_buf = std::path::Path::new(UPLOADS_DIRECTORY).join(&path);
+        let mut image_file = BufReader::new(File::open(path_buf).await?);
 
-    let filename_latest = "aaa-latest.jpg";
-    let path_latest = format!("{}/{}", serial_number, filename_latest);
+        let filename_latest = "aaa-latest.jpg";
+        let path_latest = format!("{}/{}", serial_number, filename_latest);
 
-    // Create the file. `File` implements `AsyncWrite`.
-    let path_latest_buf = std::path::Path::new(UPLOADS_DIRECTORY).join(&path_latest);
-    let mut file_latest = BufWriter::new(File::create(path_latest_buf).await?);
+        // Create the file. `File` implements `AsyncWrite`.
+        let path_latest_buf = std::path::Path::new(UPLOADS_DIRECTORY).join(&path_latest);
+        let mut file_latest = BufWriter::new(File::create(path_latest_buf).await?);
 
-    // Copy the image file into the latest file.
-    tokio::io::copy(&mut image_file, &mut file_latest).await?;
-    tracing::debug!(
-        "image saved to {}: {} and {}",
-        serial_number,
-        filename,
-        filename_latest
-    );
+        // Copy the image file into the latest file.
+        tokio::io::copy(&mut image_file, &mut file_latest).await?;
+        tracing::debug!(
+            "image saved to {}: {} and {}",
+            serial_number,
+            filename,
+            filename_latest
+        );
+
+        Ok::<_, io::Error>(())
+    }
+    .await
+    .map_err(|err| err.to_string())
 }
